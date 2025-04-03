9a880d7289fd9bac4e9b92470740b932a6db4963
acoustic0322 <sounds_on_street@yahoo.co.jp>
2025-04-04 01:37:38 +0900

    ポスト無効にしていた

--- DbotManager/TweetTask.cs
+++ DbotManager/TweetTask.cs
@@ -917,7 +917,6 @@ namespace DbotManager
             {
                 case TweetProcTypes.POST:
 
-                    return null;
                     pythonScriptPath += $" mode={GetTweetMode(tweetCommand.TweetProcType)} account_id={tweetCommand.AccountId} comment_id={tweetCommand.CommentId} ai_enable={tweetCommand.AiEnable}";
 
                     if(tweetCommand.MediaType != MediaTypes.None)
