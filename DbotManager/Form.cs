e6e0ecd8607fc8f612b54a85a9fa9d71e65afb48
MT4ver2-a03-0IVGULYn <MT4ver2-a03-0IVGULYn@FDC24145105204>
2026-06-24 07:33:20 +0900

    修正

--- DbotManager/Form.cs
+++ DbotManager/Form.cs
@@ -585,9 +585,17 @@ namespace DbotManager
 
             var userRowWk = userList.Where(x => x.Id == userId).FirstOrDefault();
 
-            _tweetTask.AdminMode = userRowWk.Admin;
+            if(userRowWk is null)
+            {
+                _tweetTask.AdminMode = false;
+            }
+            else
+            {
+                _tweetTask.AdminMode = userRowWk.Admin;
+            }
 
-            if(_tweetTask.SensyukenMode)
+
+            if (_tweetTask.SensyukenMode)
             {
                 if (userRowWk is null || userRowWk.Admin)
                 {
